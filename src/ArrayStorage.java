fa8b0761b8933ac197bf032eb80ddc08c343dcf5
TYM <artymk77@gmail.com>
2020-05-28 12:38:50 +0300

    realised methods in ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,64 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        storage = null;
     }
 
     void save(Resume r) {
+        for (Resume resumeFromStorage : storage) {
+            if (resumeFromStorage != null) {
+                resumeFromStorage = r;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume uuidGet = null;
+        try {
+            for (Resume resumeFromStorage : storage) {
+                if (resumeFromStorage.equals(uuid)) {
+                    uuidGet = resumeFromStorage;
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println("искомое резюме отсутсвует");
+        }
+        return uuidGet;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (storage[i].equals(uuid)) {
+                storage[i] = storage[i + 1];
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+
+        Resume[] resumeNotNull = null;
+        try {
+            for (int i = 0; i < storage.length; i++) {
+                if (storage[i] != null) {
+                    resumeNotNull[i] = storage[i];
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println("база резюме пустая!");
+        }
+        return resumeNotNull;
     }
 
     int size() {
-        return 0;
+        int countResumeInStorage = 0;
+        for (Resume resumeFromStorage : storage) {
+            if (resumeFromStorage != null) {
+                countResumeInStorage++;
+            }
+        }
+        return countResumeInStorage;
     }
 }
