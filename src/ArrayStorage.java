a0377f83c01366d28b19d877f196d74ed2ef29d8
TYM <artymk77@gmail.com>
2020-05-28 20:51:00 +0300

    fixed realisation all methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,66 +3,60 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int sizeResume;
 
     void clear() {
-        storage = null;
+        for (Resume resume : storage) {
+            if (resume != null) {
+                resume = null;
+            }
+        }
+        sizeResume = 0;
     }
 
     void save(Resume r) {
-        for (Resume resumeFromStorage : storage) {
-            if (resumeFromStorage != null) {
-                resumeFromStorage = r;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
             }
         }
+        sizeResume++;
     }
 
     Resume get(String uuid) {
         Resume uuidGet = null;
-        try {
-            for (Resume resumeFromStorage : storage) {
-                if (resumeFromStorage.equals(uuid)) {
-                    uuidGet = resumeFromStorage;
-                }
+        for (int i = 0; i < sizeResume; i++) {
+            if (storage[i].toString() == uuid) {
+                uuidGet = storage[i];
+                break;
             }
-        } catch (Exception ex) {
-            System.out.println("искомое резюме отсутсвует");
         }
         return uuidGet;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < storage.length - 1; i++) {
+        for (int i = 0; i < sizeResume; i++) {
             if (storage[i].equals(uuid)) {
-                storage[i] = storage[i + 1];
+                for (int j = i; j < sizeResume; j++) {
+                    storage[j] = storage[j + 1];
+                }
+                break;
             }
         }
+        sizeResume--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-
-        Resume[] resumeNotNull = null;
-        try {
-            for (int i = 0; i < storage.length; i++) {
-                if (storage[i] != null) {
-                    resumeNotNull[i] = storage[i];
-                }
-            }
-        } catch (Exception ex) {
-            System.out.println("база резюме пустая!");
-        }
+        Resume[] resumeNotNull = new Resume[sizeResume];
+        System.arraycopy(storage, 0, resumeNotNull, 0, sizeResume);
         return resumeNotNull;
     }
 
     int size() {
-        int countResumeInStorage = 0;
-        for (Resume resumeFromStorage : storage) {
-            if (resumeFromStorage != null) {
-                countResumeInStorage++;
-            }
-        }
-        return countResumeInStorage;
+        return sizeResume;
     }
 }
