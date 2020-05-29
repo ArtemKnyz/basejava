3a0dac6b76877f9af262facbd09fe013350b1d96
TYM <artymk77@gmail.com>
2020-05-29 12:21:09 +0300

    1. fixed error when lenth = 3; 2. removed in "sizeResume--;" in rigthPlace 3. renamed variable "Resume uuidGet" as just resume 4. injected check on null in save()  method

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,50 +1,54 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    Resume[] storage = new Resume[1000];
     int sizeResume;
 
     void clear() {
-        for (Resume resume : storage) {
-            if (resume != null) {
-                resume = null;
-            }
+        for (int i = 0; i < sizeResume; i++) {
+            storage[i] = null;
         }
         sizeResume = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                break;
+        if (Arrays.asList(storage).contains(null)) {
+            for (int i = 0; i < storage.length; i++) {
+                if (r != null && storage[i] == null) {
+                    storage[i] = r;
+                    sizeResume++;
+                    break;
+                }
             }
+        } else {
+            System.out.println("Массив резюме заполнен, очистите перед добавлением " + r);
         }
-        sizeResume++;
     }
 
     Resume get(String uuid) {
-        Resume uuidGet = null;
+        Resume resume = null;
         for (int i = 0; i < sizeResume; i++) {
             if (storage[i].toString() == uuid) {
-                uuidGet = storage[i];
+                resume = storage[i];
                 break;
             }
         }
-        return uuidGet;
+        return resume;
     }
 
     void delete(String uuid) {
         for (int i = 0; i < sizeResume; i++) {
             if (storage[i].toString() == uuid) {
-                for (int j = i; j < sizeResume; j++) {
+                for (int j = i; j < sizeResume - 1; j++) {
                     storage[j] = storage[j + 1];
                 }
+                sizeResume--;
                 break;
             }
         }
-        sizeResume--;
     }
 
     /**
